a28f33757cc06b2e4f9a76e261e0fa62ca0088ce
wannnwdooo <wannnwdooo@gmail.com>
2022-05-16 16:35:30 +0300

    foxed error end deploy

--- src/Themes.js
+++ src/Themes.js
@@ -5,14 +5,14 @@ export const lightTheme = {
   container: "#EFDDF6",
   border: "2px solid #EBE4EB",
   fontColor: "black",
-  shadow: "12px 12px 2px 1px rgba(57, 13, 217, 0.15)",
+  shadow: "12px 12px 2px 1px rgba(57, 13, 217, 0.15)"
 };
 export const darkTheme = {
   body: "linear-gradient(to right bottom, #bd7e88, #6759ab)",
   container: "rgba(24, 24, 24, 0.82)",
   border: "2px solid #c5c3c3",
   fontColor: "white",
-  shadow: "12px 12px 2px 1px rgba(81, 103, 204, 0.3)",
+  shadow: "12px 12px 2px 1px rgba(81, 103, 204, 0.3)"
 };
 
 export const GlobalStyles = createGlobalStyle`
