415aa1d298636837384634463dc055c929737e21
wannnwdooo <wannnwdooo@gmail.com>
2022-05-21 17:49:30 +0300

    change from https to http due to API developer deprecation

--- src/API/cityApi.js
+++ src/API/cityApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const cityApi = async (city) => {
-  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=`;
+  const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=`;
   const apiKey = "9995ad9e9c273c0c3114083c55d0ccaf";
   const url = `${apiUrl}${apiKey}`;
 
